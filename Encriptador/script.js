e89bc9173f6594bcce9b099ae934dd0834632485
ADMIN <lemanerp@gmail.com>
2023-08-29 20:00:06 -0600

    desencriptador actualizado java

--- Encriptador/script.js
+++ Encriptador/script.js
@@ -29,6 +29,31 @@
 
 }
  
+ function desencriptar(){
+   let validartexto = document.getElementById("validartexto").value;
+   let tituloMensaje=document.getElementById("titulo-mensaje");
+    let parrafo=document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco")
+   let textoCifrado = validartexto
+   .replace(/enter/img, "e")
+   .replace(/ober/img, "o")
+   .replace(/imes/img, "i")
+   .replace(/ai/img, "a")
+   .replace(/ufat/img, "u")
+
+   if(validartexto.length !=0){
+      document.getElementById("validartexto").value = textoCifrado; 
+      tituloMensaje.textContent = "Texto desencriptado con exito";
+      parrafo.textContent ="";
+      muñeco.src = "./imagenes/encriptado.jpg";
+  } else{
+      muñeco.src ="./imagenes/muñeco.png";
+       tituloMensaje.textContent = "Ningún mensaje fue encontrado"
+         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
+         alert("Debes ingresar algún texto");
+  }
+
+ }
  
  
  
@@ -36,74 +61,3 @@
  
  
  
- //const textoValidar = document.querySelector(".textarea");
- //const mensaje = document.querySelector(".mensaje");
- //const copia = document.querySelector(".copiar");
- //copia.style.display = "none"
-
-
- //function validartexto(){
-    // let textoEscrito = document.querySelector(".textarea").value;
-     //let validar = textoEscrito.match(/^[a-z]*$/); 
-
-        // if(!validar || validar === 0 ){
-
-           //  alert("Solo son permitidas letras minúsculas y sin acentos")
-             //location.reload(); //método para recargar la URL
-             //return true;
-        // }
- //}
-
-//function btnEncriptar(){
-  //  if(!validartexto()) {
-    ///    const textoEncriptado = encriptar(textoValidar.value)
-       // mensaje.value = textoEncriptado
-        // mensaje.style.backgroundImage = "none"
-         //textoValidar.value = "";
-        // copia.style.display = "block"
-    
-    // }
- //}
-// `La letra "e" es convertida para "enter"`
-// `La letra "i" es convertida para "imes"`
-// `La letra "a" es convertida para "ai"`
-// `La letra "o" es convertida para "ober"`
-// `La letra "u" es convertida para "ufat"`
-
-
- //function encriptar(stringEncriptada){
-   //  let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    // stringEncriptada = stringEncriptada.toLowerCase()
-
-     //for(let i = 0; i < matrizCodigo.length; i++){
-         //if(stringEncriptada.includes(matrizCodigo[i][0])){
-          //   stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
-
-        // }
-
-     //}
-     //return stringEncriptada
- //}
- //function btnDesencriptar(){
-    // const textoEncriptado = desencriptar(textarea.value)
-    // mensaje.value = textoEncriptado
-    // textarea.value = "";
-    
- //}
-
-
- //function desencriptar(stringDesencriptada){
-    // let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-     //stringDesencriptada = stringDesencriptada.toLowerCase()
-
-     //for(let i = 0; i < matrizCodigo.length; i++){
-        // if(stringDesencriptada.includes(matrizCodigo[i][1])){
-           //  stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1] , matrizCodigo[i][0])
-
-         //}
-
-     //}
-     //return stringDesencriptada
- //}
-
-
